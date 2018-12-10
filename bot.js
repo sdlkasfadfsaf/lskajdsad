1be81f1ef1b30c9791b780b566ef51908f3d9d2a
sdlkasfadfsaf <42405837+sdlkasfadfsaf@users.noreply.github.com>
2018-12-10 09:58:41 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,7 +210,45 @@ client.on('message', message => {
     }
 });
 
-
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("514950689508818992");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("516764647987216422");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("514950689508818992");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`invited BY ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 
 
